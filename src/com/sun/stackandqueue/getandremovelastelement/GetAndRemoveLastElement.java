9175ebbf5f1c85c01dabddd5eb80d08da2062b77
1365037264@qq.com <1365037264@qq.com>
2019-04-09 19:30:35 +0800

    update
    Signed-off-by: 1365037264@qq.com <1365037264@qq.com>

--- src/com/sun/stackandqueue/getandremovelastelement/GetAndRemoveLastElement.java
+++ src/com/sun/stackandqueue/getandremovelastelement/GetAndRemoveLastElement.java
@@ -1,9 +1,41 @@
 package com.sun.stackandqueue.getandremovelastelement;
 
+import java.util.Stack;
+
 public class GetAndRemoveLastElement {
 
-    public static int  getAndRemoveLastElement() {
-        int result = stack.
+    public static int getAndRemoveLastElement(Stack<Integer> stack) {
+        int result = stack.pop();
+        if (stack.isEmpty()) {
+            return result;
+        } else {
+            int last = getAndRemoveLastElement(stack);
+            stack.push(result);
+            return last;
+        }
+    }
+    public static void reverse(Stack<Integer> stack) {
+        if(stack.isEmpty()){
+            return;
+        }
+        int i = getAndRemoveLastElement(stack);
+        reverse(stack);
+        stack.push(i);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        for (Integer num : stack) {
+            System.out.print(num);
+        }
+        System.out.println();
+        reverse(stack);
+        for (Integer num : stack) {
+            System.out.print(num);
+        }
     }
 
 }
